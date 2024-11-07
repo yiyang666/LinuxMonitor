bdacc663fe25d533722500d4ac5a146ab2e5e531
young <beyondyiyang@qq.com>
2024-11-08 00:26:15 +0800

    add note 11/7

--- docker/scripts/monitor_docker_into.sh
+++ docker/scripts/monitor_docker_into.sh
@@ -7,6 +7,14 @@
 # docker start "容器id" 启动一个已有容器,分配默认终端
 # docler stop "容器id"  关闭一个已有容器
 
+# xhost +local:root指定本地root用户才能访问Xserver
+# exec会以root的身份在容器内开启一个终端
+# 这个终端就有了范围主机Xserver的权限
+# 再把这个权限在本地回收，容器内的终端不影响
+
+# 1>/dev/null 表示终端输出的重定向到 /dev/null（特殊目录，用来丢弃一些输出）
+# 2>&1 表示把标准错误重定向到标准输出相同的地址上
+
 xhost +local:root 1>/dev/null 2>&1
 docker exec \
     -u root \
